de04bd8c92927ed1d9dd36eefbbf05fde99f56f2
phuc <vanphucpro123@gmail.com>
2020-07-08 16:01:19 +0700

    creat script download

---
+++ data_download.py
@@ -0,0 +1,17 @@
+import os
+import urllib.request
+import tarfile
+
+data_dir = "./data"
+
+if not os.path.exists(data_dir):
+    os.mkdir(data_dir)
+
+url = "http://pjreddie.com/media/files/VOCtrainval_11-May-2012.tar"
+target_path = os.path.join(data_dir, "VOCtrainval_11-May-2012.tar")
+
+if not os.path.exists(target_path):
+    urllib.request.urlretrieve(url, target_path)
+    tar = tarfile.TarFile(target_path)
+    tar.extractall(data_dir)
+    tar.close()
